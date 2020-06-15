0c5f47b95b51bfd4aa6469a9d4704da66b725739
Archit Shah <archit0994@gmail.com>
2020-06-15 06:46:56 -0400

    Added the additional task model to Bootapp

--- Bootapp/admin.py
+++ Bootapp/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from .models import info
-# Register your models here.
+from .models import info, customer,city,restaurants
+
 admin.site.register(info)
+admin.site.register(customer)
+admin.site.register(city)
+admin.site.register(restaurants)
--- Bootapp/models.py
+++ Bootapp/models.py
@@ -6,3 +6,24 @@ class info(models.Model):
     id=models.AutoField(primary_key=True)
     contact=models.CharField(max_length=10)
     address=models.CharField(max_length=250) 
+
+class city(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    city_name = models.CharField(max_length=255)
+    zip_code = models.CharField(max_length=16)
+
+class customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=255)
+    city_id = models.ForeignKey(city, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    contact_phone = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    confirmation_code = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    time_joined = models.DateTimeField()
+    
+class restaurants(models.Model):
+    restaurant_id = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=255)
+    city_id = models.ForeignKey(city,on_delete=models.CASCADE)
